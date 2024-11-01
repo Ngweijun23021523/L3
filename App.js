d6e3aa48da899ed1164837925f66a768dacea308
ngweijun <23021523@myrp.edu.sg>
2024-11-01 14:09:31 +0800

    First push

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, TextInput, Alert, Button, TouchableOpacity,ToastAndroid} from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
+import React, {useState} from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const InputBox = ({label, onChangeText})=> {
+    return (
+        <View>
+            <Text>{label}</Text>
+            <TextInput style={{borderWidth: 1}} onChangeText={onChangeText} />
+        </View>
+    );
+};
+
+
+
+const MyApp = () => {
+    const[pw,setPw] = useState("");
+    const[Name, setName] = useState("");
+    const[UserType, setUserType] = useState("");
+    return (
+        <View style={{padding: 20, paddingTop:50}}>
+            <Text>User type:</Text>
+            <RNPickerSelect
+            onValueChange={(value) => setUserType(value)}
+            items={[
+                { label: 'Admin', value: 'Admin' },
+                { label: 'Guest', value: 'Guest' },
+                { label: 'User', value: 'User' },
+            ]}/>
+
+            <InputBox label="User Name:" onChangeText={(text) => setName(text)}/>
+            <InputBox label="Password:" onChangeText={(text) => setPw(text)}/>
+
+
+
+
+            <TouchableOpacity
+                onPress={() => ToastAndroid.show("Welcome!"+'  '+ UserType+'  '+ Name ,
+                    ToastAndroid.SHORT)}>
+
+                <Text>Login</Text>
+            </TouchableOpacity>
+
+        </View>
+    );
+};
+//onPress={()  => Alert.alert("Welcome!")}>
+
+// Correct export statement
+export default MyApp;
+
+
+
